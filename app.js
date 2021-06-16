3395875a71bf90a9ed6995995dfadd465a55f5c6
arjundashrath <54043589+arjundashrath@users.noreply.github.com>
2021-06-16 23:48:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+//NODE JS CLI NOTES APPLICATION
+//Made with <3 by Arjun Dashrath
+
 const notes = require('./notes.js')
 const chalk = require('chalk')
 const yargs = require('yargs')
-yargs.version("1.0.0")
+const prompt = require('prompt')
+yargs.version("2.0.0")
 yargs.command({
 	command: 'add',
 	describe: 'Add a new note',
@@ -9,18 +13,37 @@ yargs.command({
 		title:
 		{
 			describe: 'Title of note',
-			demandOption: true,
+			demandOption: false,
 			type: 'string'
 		},
 		body:
 		{
 			describe: 'Body of note',
-			demandOption: true,
+			demandOption: false,
 			type: 'string'
 		}
 	},
 	handler(argv){
-		notes.addNote(argv.title, argv.body)
+		
+		if(!argv.title && !argv.body)
+		{
+			prompt.start();
+			prompt.get(['notetitle','notebody'],(err,result)=>{
+			notes.addNote(result.notetitle, result.notebody)})
+		}
+		else if(!argv.body && argv.title.length > 0)
+		{
+			prompt.start();
+			prompt.get(['notebody'],(err,result)=>{
+			notes.addNote(argv.title, result.notebody)})
+		}
+		else if(!argv.title && argv.body.length > 0)
+		{
+			prompt.start();
+			prompt.get(['notetitle'],(err,result)=>{
+			notes.addNote(result.notetitle, argv.body)})
+		}
+		
 	}
 })
 
@@ -30,11 +53,15 @@ const remfun = function(){
 yargs.command({
 	command: 'remove',
 	describe: 'remove note',
-	handler(argv){notes.removeNote(argv.title)},
+	handler(argv){
+	prompt.start();
+		prompt.get(['index'],(err,result)=>{
+		notes.removeNote(result.index)})
+	},
 	builder: {
 		title:{
 			describe: "Note title",
-			demandOption: true,
+			demandOption: false,
 			type: 'string'
 		}
 	}
